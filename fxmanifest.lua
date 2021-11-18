c9b573160d6b5b2d533118493d7dfab6e30bf341
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-11-18 11:47:33 +0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,9 @@
-fx_version 'cerulean'
-games { 'rdr3', 'gta5' }
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+fx_version 'adamant'
+game 'gta5'
+name "ak47_gangs"
+author "MenanAk47 (MenanAk47#3129)"
+description "MenanAk47 - Optimized Gangs"
+version "1.0"
 
 ui_page {
     'nui/index.html',
